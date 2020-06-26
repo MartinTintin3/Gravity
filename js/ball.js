2bc4129e1c8e27541f6e83d5ffa0f0f51070e8ab
martinGITHUBER <maroymart@gmail.com>
2020-06-26 16:15:57 -0400

    Update ball.js

--- js/ball.js
+++ js/ball.js
@@ -25,7 +25,7 @@ function Ball(x, y, radius, color){
   }
 
   this.move = function(){
-    if((this.x + this.radius <= 0 && left == true) || (this.x >= canvas.width - this.radius && left == true)){
+    if((this.x - this.radius <= 0 && this.velocity.x < 0) || (this.x >= canvas.width - this.radius && this.velocity.x > 0)){
       this.velocity.x -= Math.round(this.velocity.x / 2);
       this.velocity.x *= -1;
     }
